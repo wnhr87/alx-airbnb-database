8a579b61fda3c0f1c7fef44572a4865d3ffbc863
wnhr87 <wnhr.11pt@gmail.com>
2025-07-05 03:19:52 +0000

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -13,15 +13,31 @@ INNER JOIN
 
 --LEFT JOIN: Properties and Reviews
 --This retrieves all properties, including those without any reviews.
-SELECT 
-    properties.property_id,
-    properties.name,
-    reviews.review_id,
-    reviews.rating
-FROM 
-    properties
-LEFT JOIN 
-    reviews ON properties.property_id = reviews.property_id;
+--SELECT 
+  --  properties.property_id,
+  --  properties.name,
+   -- reviews.review_id,
+   -- reviews.rating
+--FROM 
+   -- properties
+--LEFT JOIN 
+  --  reviews ON properties.property_id = reviews.property_id;
+SELECT
+  p.id          AS property_id,
+  p.name        AS property_name,
+  p.location,
+  r.id          AS review_id,
+  r.user_id     AS reviewer_id,
+  r.rating,
+  r.comment
+FROM
+  properties AS p
+LEFT JOIN
+  reviews AS r
+ON
+  p.id = r.property_id;
+
+
 
 --FULL OUTER JOIN: Users and Bookings
 --Shows all users and all bookings—even if there’s no match between them.

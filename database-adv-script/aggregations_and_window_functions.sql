362b249622784d82e4ecc30402893ce1447b4fd5
wnhr87 <wnhr.11pt@gmail.com>
2025-07-05 03:29:31 +0000

    Add files via upload

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,48 +1,35 @@
--- Total Number of Bookings per User
+-- Write a query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause.
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    COUNT(b.booking_id) AS total_bookings
+FROM Users u
+LEFT JOIN Booking b ON u.user_id = b.user_id
+GROUP BY u.user_id, u.first_name, u.last_name, u.email;
 
 
-SELECT
-  u.id               AS user_id,
-  u.username         AS username,
-  COUNT(b.id)        AS total_bookings
-FROM
-  users AS u
-LEFT JOIN
-  bookings AS b
-ON
-  u.id = b.user_id
-GROUP BY
-  u.id,
-  u.username
-ORDER BY
-  total_bookings DESC;
+-- Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
 
+-- SELECT 
+--     p.property_id,
+--     p.name,
+--     p.location,
+--     p.pricepernight,
+--     COUNT(b.booking_id) AS total_bookings,
+--     RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+-- FROM Property p
+-- LEFT JOIN Booking b ON p.property_id = b.property_id
+-- GROUP BY p.property_id, p.name, p.location, p.pricepernight;
 
--- Rank Properties by Total Bookings
-
-WITH property_counts AS (
-  SELECT
-    p.id        AS property_id,
-    p.name      AS property_name,
-    COUNT(b.id) AS total_bookings
-  FROM
-    properties AS p
-  LEFT JOIN
-    bookings AS b
-  ON
-    p.id = b.property_id
-  GROUP BY
-    p.id,
-    p.name
-)
-SELECT
-  property_id,
-  property_name,
-  total_bookings,
-  RANK() OVER (
-    ORDER BY total_bookings DESC
-  ) AS booking_rank
-FROM
-  property_counts
-ORDER BY
-  booking_rank;
+SELECT 
+    p.property_id,
+    p.name,
+    p.location,
+    p.pricepernight,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_number
+FROM Property p
+LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name, p.location, p.pricepernight;
